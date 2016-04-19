d46d864f64515fd7fe715260760032fab25dc9d0
Lars Petersen <larsp.26@web.de>
2016-04-19 10:13:05 +0200

    fixed #15 not an issue

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.php">Solve IT yourself!</a>
+                    <a class="navbar-brand" href="index.php">Solve IT yourself!</a> 
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
