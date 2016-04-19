ef23be141b7e5181f152adbdd9e59f690f5fc487
Oliver Reipschlaeger <thepsi43@gmail.com>
2016-04-19 10:54:19 +0200

    MOD includes, contact, index
    FIX database connection on includes.php
    
    Fixes #27

--- includes.php
+++ includes.php
@@ -1,6 +1,11 @@
 <?php
-require_once 'lib/config.php';
-require_once 'lib/db.php';
-$db = new db($config['dbhost'], $config['dbname'], $config['dbuser'], $config['dbpassword']);
+$filepath = "/lib/config.php";
+if(file_exists($filepath)) {
+    require_once 'lib/config.php';
+    require_once 'lib/db.php';
+    $db = new db($config['dbhost'], $config['dbname'], $config['dbuser'], $config['dbpassword']);
+} else {
+    echo "The connection to the database could not be established because important files are missing. ";
+}
 //$result = $db->query('SELECT * FROM tree');
 ?>
