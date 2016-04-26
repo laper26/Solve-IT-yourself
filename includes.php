9762fb777e9a752f2e59af0e43a1b7d2772518d0
Lars Peter Søndergaard <lars.peter@sondergaard.de>
2016-04-26 10:12:02 +0200

    Refactor project structure to use template engine

--- includes.php
+++ includes.php
@@ -1,11 +1,15 @@
 <?php
-$filepath = "/lib/config.php";
+
+header('Content-Type: text/html; charset=utf-8');
+$filepath = "lib/config.php";
+
 if(file_exists($filepath)) {
-    require_once 'lib/config.php';
+    require_once $filepath;
     require_once 'lib/db.php';
     $db = new db($config['dbhost'], $config['dbname'], $config['dbuser'], $config['dbpassword']);
 } else {
     echo "The connection to the database could not be established because important files are missing. ";
+    die();
 }
-//$result = $db->query('SELECT * FROM tree');
-?>
+
+require_once 'lib/template.php';
